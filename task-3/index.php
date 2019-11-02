5be3182769502c46f0d2dd77968895ba3ae09597
Yaskovich Dmitry <kadavr95@outlook.com>
2019-11-03 00:00:00 +0300

    filenames localization fixes

--- task-3/index.php
+++ task-3/index.php
@@ -1,7 +1,6 @@
 <?php 
 unlink( "file.txt" ); // Удаление файла
- rmdir( "papka1/" ); // Удаление директории. Внимание, папка должна быть пустой 
- copy( "otkuda.txt" , "kuda.txt" ); // Копирование. 
- rename( "chto.txt" , "vo_chto.txt" ); // Переименование. 
-
+ rmdir( "folder1/" ); // Удаление директории. Внимание, папка должна быть пустой 
+ copy( "from.txt" , "to.txt" ); // Копирование. 
+ rename( "old_name.txt" , "new_name.txt" ); // Переименование. 
 ?>
