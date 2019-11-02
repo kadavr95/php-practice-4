716edfb104f04ccf551420c51fad9e16b5541394
Yaskovich Dmitry <kadavr95@outlook.com>
2019-11-03 00:00:15 +0300

    support for unicode characters

--- task-5/index.php
+++ task-5/index.php
@@ -1,10 +1,10 @@
 <?php
 $str_1="Первая строка "; 
-echo 'Длина первой строки = '.strlen($str_1).'<br>'; 
+echo 'Длина первой строки = '.mb_strlen($str_1).'<br>'; 
 $str_2="Вторая строка"; 
-echo 'Длина второй строки = '.strlen($str_2).'<br>';
+echo 'Длина второй строки = '.mb_strlen($str_2).'<br>';
 $str_3='строка';// подстрока
-$p=strstr($str_1,$str_3);
+$p=mb_strstr($str_1,$str_3);
 echo 'поиск подстроки:  '.$p.'<br>';
 $sr=strcmp($str_1,$str_2 );// сравнение строк
 echo $sr;
